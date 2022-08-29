b5d07023f539b32fd8878f90e64c87bdce20f7e6
juanlopes <juanlopes@MacBook-Air-de-Juan.local>
2022-08-29 13:49:04 -0300

    First Commit

---
+++ TesteSenior/Classes/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TesteSenior.Classes
+{
+	public class Node
+	{
+		public int value { get; set; }
+		private List<int> connections { get; set; }
+
+		public Node(int initializer)
+		{
+			value = initializer;
+			connections = new List<int>();
+		}
+
+		public void setNewConnection(int nodeValue)
+        {
+			connections.Add(nodeValue);
+        }
+
+		public List<int> getConnections()
+        {
+			return connections;
+        }
+	}
+}
+
